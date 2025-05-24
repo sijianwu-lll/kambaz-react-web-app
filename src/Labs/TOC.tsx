558b4aeb16e9e20229e9881b151bec98445b9ffc
Sijian Wu <you@example.comls>
2025-05-24 16:14:52 +0800

    fix labs incorrect nagivation problem

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,26 +1,51 @@
 import Nav from "react-bootstrap/Nav";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function TOC() {
+  const location = useLocation();
+
   return (
     <Nav variant="pills">
       <Nav.Item>
-        <Nav.Link to="/Labs" as={Link}>Lab 1</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab1"
+          active={location.pathname.includes("Lab1")}
+        >
+          Lab 1
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="/Labs/Lab1" as={Link}>Lab 1</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab2"
+          active={location.pathname.includes("Lab2")}
+        >
+          Lab 2
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="/Labs/Lab2" as={Link} active>Lab 2</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab3"
+          active={location.pathname.includes("Lab3")}
+        >
+          Lab 3
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="/Labs/Lab3" as={Link}>Lab 3</Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/Kambaz"
+          active={location.pathname.includes("Kambaz")}
+        >
+          Kambaz
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link to="/Kambaz" as={Link}>Kambaz</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
+        <Nav.Link href="https://github.com/jannunzi" target="_blank">
+          My GitHub
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
